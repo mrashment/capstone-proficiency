2a452b0465efab990c6144ba25550a0fda425eec
Mason Ashment <mashment@iu.edu>
2019-10-02 17:56:00 -0400

    initialized variables

--- proficiency-01/app/src/main/java/com/gmail/masonashment/proficiency01/MainActivity.java
+++ proficiency-01/app/src/main/java/com/gmail/masonashment/proficiency01/MainActivity.java
@@ -2,7 +2,13 @@ package com.gmail.masonashment.proficiency01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get the current time
+        Date currentTime = Calendar.getInstance().getTime();
+
+        //create variable to reference textview
+        TextView messageTextView = (TextView) findViewById(R.id.messageTextview);
+
+        //set the textviews message
+        messageTextView.setText(currentTime.toString());
+
     }
 }
