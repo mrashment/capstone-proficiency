8e4b4accb0a3f6da2b629407603767a5f5c6cbc1
Mason Ashment <masonashment@gmail.com>
2019-10-09 17:29:54 -0400

    more comments changed some formatting in the layout

--- proficiency-01/README.md
+++ proficiency-01/README.md
@@ -1,1 +1,2 @@
 # Proficiency 01
+This is an app that displays a custom message depending on the time of day. I used a SimpleDateFormat and instances of the Date class to compare set times to the current time. 
--- proficiency-01/app/src/main/java/com/gmail/masonashment/proficiency01/MainActivity.java
+++ proficiency-01/app/src/main/java/com/gmail/masonashment/proficiency01/MainActivity.java
@@ -2,11 +2,9 @@ package com.gmail.masonashment.proficiency01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -23,15 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get the current time
+        // how I want the time to be displayed
         SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-        String formatter = "oops";
+        String formatter = "oops"; //coming back to this I have no idea why I wrote oops, but I'm assuming it's essential
+        // get current time
         Date currentTime = Calendar.getInstance().getTime();
 
         try {
+            // set times so I can compare current time to them later
             noon = timeFormat.parse("12:00:00");
             afternoonEnd = timeFormat.parse("18:00:00");
             earlyMorn = timeFormat.parse("05:45:00");
+            // this ensures the current time is in the same format as the others
             formatter = timeFormat.format(currentTime);
             current = timeFormat.parse(formatter);
 
@@ -39,20 +40,23 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        //create variable to reference textviews
-        TextView timeTextView = (TextView) findViewById(R.id.timeTextview);
-        TextView messageTextView = (TextView) findViewById(R.id.messageTextview);
+        // create variable to reference textviews
+        TextView timeTextView = findViewById(R.id.timeTextview);
+        TextView messageTextView = findViewById(R.id.messageTextview);
 
-        //set the textviews messages
+        // set the textviews messages
         timeTextView.setText(formatter);
+        // check if its the morning
         if(current.equals(earlyMorn) ||
                 (current.after(earlyMorn) && current.before(noon))) {
             messageTextView.setText("Good Morning!");
         }
+        // afternoon
         else if(current.equals(noon) ||
                 (current.after(noon) && current.before(afternoonEnd))) {
             messageTextView.setText("Good Afternoon!");
         }
+        // evening
         else {
             messageTextView.setText("Good Evening!");
         }
