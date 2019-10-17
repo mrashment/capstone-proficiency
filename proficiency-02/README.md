17ea4185010379497606cc3c821ba362ae23ffce
Mason Ashment <mashment@iu.edu>
2019-10-16 23:19:11 -0400

    set up onClickListeners for buttons

--- proficiency-02/README.md
+++ proficiency-02/README.md
@@ -1,1 +1,2 @@
 # Proficiency 02
+A simple calculator set up with button that have onClickListeners for user input.
--- proficiency-02/app/src/main/java/com/gmail/masonashment/myapplication/MainActivity.java
+++ proficiency-02/app/src/main/java/com/gmail/masonashment/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.gmail.masonashment.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView resultTextView;
+    private EditText numberEditText;
     private Button button0;
     private Button button2;
     private Button button4;
@@ -23,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // initialize all the buttons
+        resultTextView = findViewById(R.id.resultTextView);
+        numberEditText = findViewById(R.id.numberEditText);
         button0 = findViewById(R.id.button0);
         button2 = findViewById(R.id.button2);
         button4 = findViewById(R.id.button4);
@@ -33,6 +39,47 @@ public class MainActivity extends AppCompatActivity {
         buttonEquals = findViewById(R.id.buttonEquals);
         buttonClear = findViewById(R.id.buttonClear);
 
+        // number will be appended to the numberEditText
+        View.OnClickListener numberListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //append the number
+                Button button = (Button) view;
+                numberEditText.append(button.getText().toString());
+            }
+        };
+
+        // set on click listeners for numbers
+        button0.setOnClickListener(numberListener);
+        button2.setOnClickListener(numberListener);
+        button4.setOnClickListener(numberListener);
+        button6.setOnClickListener(numberListener);
+        button8.setOnClickListener(numberListener);
+
+        // clear button erases all text
+        View.OnClickListener clearListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberEditText.setText("");
+                resultTextView.setText("");
+            }
+        };
+        buttonClear.setOnClickListener(clearListener);
+
+        // listener for the operation buttons
+        View.OnClickListener operationListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                String operation = button.getText().toString();
+                String value = numberEditText.getText().toString();
+                // value == number in editText, operation == symbol on button
+                performOperation(value,operation);
+            }
+        };
+    }
+
+    private void performOperation(String value, String operation) {
 
     }
 
